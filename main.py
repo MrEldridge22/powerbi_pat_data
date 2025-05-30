9058bc5c4d84e0cf7095512870ec36663ae20a76
David Eldridge <david.eldridge22@schools.sa.edu.au>
2025-05-30 11:09:46 +0930

    Done! Works with Reading and Maths into one output file. Should be able to handle multiple years eventually. Test this later

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import os
 import process_data
 import pandas as pd
 
-### MATHS DATA PROCESSING ###
-final_df = pd.DataFrame()
-maths_data_dir = 'raw_data\\maths'
-# Check if the directory exists
-if not os.path.exists(maths_data_dir):
-    print(f"Directory {maths_data_dir} does not exist.")
-else:
-    print(f"Directory {maths_data_dir} exists. Proceeding with data processing...")
 
+# Check if the directory exists
+data = 'raw_data'
+
+if not os.path.exists(data):
+    print(f"Directory {data} does not exist.")
+else:
+    print(f"Directory {data} exists. Proceeding with data processing...")
+
+data_df = pd.DataFrame()
 # Iterate through all Excel files in the directory
-for filename in os.listdir(maths_data_dir):
+for filename in os.listdir(data):
     if filename.endswith('.xlsx'):
-        file_path = os.path.join(maths_data_dir, filename)
+        file_path = os.path.join(data, filename)
         print(f"Processing file: {file_path}")
         # Read the Excel file
+        test = filename.split('-')[1]
         test_level = filename.split('-')[5]  # Extracting the test level from the filename
         # print(test_level)
-        final_df = process_data.process_pat_m(file_path, test_level, final_df)
+        data_df = process_data.process_pat(file_path, test, test_level, data_df)
 
     else:
         print(f"Skipping non-Excel file: {filename}")
 
-final_df.to_clipboard()
+# Output Data
+data_df.to_csv('pat_data.csv')
--- process_data.py
+++ process_data.py
@@ -1,19 +1,19 @@
 import pandas as pd
 pd.set_option('future.no_silent_downcasting', True)
 
-def process_pat_m(excel_file, test_level, df):
+def process_pat(excel_file, test, test_level, df):
     # Read the Excel file
-    patM = pd.read_excel(excel_file, header=None)
+    patData = pd.read_excel(excel_file, header=None)
     
     # Remove the first 3 rows
-    patM = patM.iloc[3:]
+    patData = patData.iloc[3:]
     # Reset the index
-    patM.reset_index(drop=True, inplace=True)
+    patData.reset_index(drop=True, inplace=True)
 
     ### Get Question Numbers and Strand Names ### 
     
     # Extract the rows where the first column value is 'Question difficulty', 'Strand', 'Precentage correct', and 'Question number'
-    question_info = patM[patM.iloc[:, 0].isin(['Question difficulty', 'Strand', 'Percentage correct', 'Question number'])]
+    question_info = patData[patData.iloc[:, 0].isin(['Question difficulty', 'Strand', 'Percentage correct', 'Question number'])]
     
     # Reset the index of the extracted DataFrame
     question_info.reset_index(drop=True, inplace=True)
@@ -40,33 +40,36 @@ def process_pat_m(excel_file, test_level, df):
         'Sp': 'Space',
         'M': 'Measurement',
         'St': 'Statistics',
-        'P': 'Probability'
+        'P': 'Probability',
+        'IE': 'Interpreting explicit information',
+        'RI': 'Retrieving directly stated information',
+        'RF': 'Reflecting on texts',
+        'II': 'Interpreting by making inferences'
     })
 
     # Keep only the Index (QuestionNumber) and the Strand columns
     question_info = question_info[['Strand']]
 
 
-
     ### Extract Student Data ###
 
     # Find the index of the row where the first column is 'Unique ID'
-    header_index = patM[patM.iloc[:, 0] == 'Unique ID'].index[0]
+    header_index = patData[patData.iloc[:, 0] == 'Unique ID'].index[0]
 
     # Slice all rows starting from the header row
-    patM_responses = patM.iloc[header_index:].reset_index(drop=True)
+    patData_responses = patData.iloc[header_index:].reset_index(drop=True)
     
     # Set the first row as header
-    patM_responses.columns = patM_responses.iloc[0]
+    patData_responses.columns = patData_responses.iloc[0]
     
     # Remove the header row from the DataFrame data
-    patM_responses = patM_responses.iloc[1:]
+    patData_responses = patData_responses.iloc[1:]
 
     # Remove columns, Family name, Given name, Middle name, Username, DOB, Gender, Year level (current), Active tags, Inactive tags, Tags (at time of test)
-    patM_responses = patM_responses.drop(columns=['Family name', 'Given name', 'Middle names', 'Username', 'DOB', 'Gender', 'Year level (current)', 'Year level (at time of test)', 'Active tags', 'Inactive tags', 'Tags (at time of test)'])
+    patData_responses = patData_responses.drop(columns=['Family name', 'Given name', 'Middle names', 'Username', 'DOB', 'Gender', 'Year level (current)', 'Year level (at time of test)', 'Active tags', 'Inactive tags', 'Tags (at time of test)'])
 
     # Change the column names after Completed and before Score using their index positions
-    columns_list = list(patM_responses.columns)
+    columns_list = list(patData_responses.columns)
     completed_index = columns_list.index("Completed")
     score_index = columns_list.index("Score")
 
@@ -74,32 +77,33 @@ def process_pat_m(excel_file, test_level, df):
     for i in range(completed_index + 1, score_index):
         columns_list[i] = str(i - completed_index)  # numeric names starting at 1 as string
 
-    patM_responses.columns = columns_list
+    patData_responses.columns = columns_list
 
     # Define the columns to keep
     id_vars = ['Unique ID', 'Completed', 'Score', 'Scale', 'Stanine', 'Percentile']
     # All remaining columns will be pivoted; these become the "QuestionNumber" column
-    value_vars = [col for col in patM_responses.columns if col not in id_vars]
+    value_vars = [col for col in patData_responses.columns if col not in id_vars]
 
     # Melt the DataFrame so that the question columns are consolidated
-    patM_melted = pd.melt(patM_responses, 
+    patData_melted = pd.melt(patData_responses, 
                         id_vars=id_vars, 
                         value_vars=value_vars, 
                         var_name='QuestionNumber', 
                         value_name='Response')
 
     # Convert Response values: NA becomes blank, ✓ becomes True, and all other values become False
-    patM_melted['Response'] = patM_melted['Response'].apply(
+    patData_melted['Response'] = patData_melted['Response'].apply(
         lambda x: "" if pd.isna(x) else (True if x == '✓' else False)
     )
 
-    # Add the test level to the melted DataFrame
-    patM_melted['Test Level'] = test_level
+    # Add the test and test level to the melted DataFrame
+    patData_melted['Test'] = test
+    patData_melted['Test Level'] = test_level
     # Add the strand information to the melted DataFrame
-    patM_melted = patM_melted.merge(question_info, left_on='QuestionNumber', right_index=True, how='left')
+    patData_melted = patData_melted.merge(question_info, left_on='QuestionNumber', right_index=True, how='left')
     # Reorder the columns
-    patM_melted = patM_melted[['Unique ID', 'Completed', 'Score', 'Scale', 'Stanine', 'Percentile', 'Test Level', 'QuestionNumber', 'Response'] + list(question_info.columns)]
+    patData_melted = patData_melted[['Unique ID', 'Test', 'Test Level', 'Completed', 'Score', 'Scale', 'Stanine', 'Percentile', 'QuestionNumber', 'Response'] + list(question_info.columns)]
 
-    df = pd.concat([df, patM_melted], ignore_index=True)
+    df = pd.concat([df, patData_melted], ignore_index=True)
     return df
 
